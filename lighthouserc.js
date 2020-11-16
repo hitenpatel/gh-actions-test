707b6b6c4967be8f9bd1e2454c7fa94329b20ffa
Hiten Patel <hiten.patel@immediate.co.uk>
2020-11-16 20:32:41 +0000

    update rc

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,19 @@
 module.exports = {
   ci: {
+    collect: {
+      numberOfRuns: 1,
+      url: ['http://localhost:3000/en'],
+      startServerCommand: 'npm start',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'offscreen-images': 'off',
+        'uses-webp-images': 'off',
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
-    },
-    collect: {
-      startServerCommand: 'npm run start',
-      startServerReadyPattern: 'ready on',
     },
   },
 };
